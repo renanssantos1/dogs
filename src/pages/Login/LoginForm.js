e2cb1ff53e96e3c5563bf468bd9715e265aec66e
Renan da silva santos <renan.serverti@gmail.com>
2020-09-06 10:19:02 -0300

    login do usuario form

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -2,11 +2,46 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function LoginForm() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((resp) => resp.json())
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
+  };
+
   return (
-    <div>
-      Login Form
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+
+        <button>Entrar</button>
+      </form>
       <Link to="/login/criar">Cadastro</Link>
-    </div>
+    </section>
   );
 }
 
