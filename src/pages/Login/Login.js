b7453e8e7d5b9ec088e323acd7a33625dcc15728
Renan da silva santos <renan.serverti@gmail.com>
2020-09-06 20:20:29 -0300

    Estilizacao do login completa

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Login.module.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginForm from "./LoginForm";
@@ -13,14 +14,16 @@ function Login() {
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 }
 
