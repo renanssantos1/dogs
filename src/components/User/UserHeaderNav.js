44ad6a4c39d70533e37abbc44681d3ccf0d8b984
Renan da silva santos <renan.serverti@gmail.com>
2020-09-08 19:37:54 -0300

    inciando os estilos

--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
+import styles from "./UserHeaderNav.module.css";
 
 import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
 import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
@@ -8,24 +9,26 @@ import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
 import { ReactComponent as Sair } from "../../Assets/sair.svg";
 
 function UserHeaderNav() {
+  const [mobile, setMobile] = React.useState(null);
+
   const { userLogout } = React.useContext(UserContext);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <NavLink to="/conta">
         <MinhasFotos />
-        Minhas Fotos
+        {mobile && " Minhas Fotos"}
       </NavLink>
       <NavLink to="/conta/estatisticas">
         <Estatisticas />
-        Estatisticas
+        {mobile && "Estatisticas"}
       </NavLink>
       <NavLink to="/conta/postar">
         <AdicionarFoto />
-        Adicionar foto
+        {mobile && "Adicionar foto"}
       </NavLink>
       <button onClick={userLogout}>
         <Sair />
-        Sair
+        {mobile && "Sair"}
       </button>
     </nav>
   );
