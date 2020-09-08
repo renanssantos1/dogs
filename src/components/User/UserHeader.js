e6faa5ca187a656c44780a53e38c41133aa6ed51
Renan da silva santos <renan.serverti@gmail.com>
2020-09-08 19:24:56 -0300

    novas paginas e rotas

---
+++ src/components/User/UserHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserHeader() {
+  return <header>Header do usuario</header>;
+}
+
+export default UserHeader;
---
+++ src/pages/User/UserPhotoPost.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserPhotoPost() {
+  return (
+    <div>
+      <h1>Photo post</h1>
+    </div>
+  );
+}
+
+export default UserPhotoPost;
