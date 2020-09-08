1200623e94d07bf91a6c4101c40dd45949fe128e
Renan da silva santos <renan.serverti@gmail.com>
2020-09-08 19:33:33 -0300

    import dos icones svg

--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -2,14 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
 
+import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
+import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
+import { ReactComponent as AdicionarFoto } from "../../Assets/adicionar.svg";
+import { ReactComponent as Sair } from "../../Assets/sair.svg";
+
 function UserHeaderNav() {
   const { userLogout } = React.useContext(UserContext);
   return (
     <nav>
-      <NavLink to="/conta">Minhas Fotos</NavLink>
-      <NavLink to="/conta/estatisticas"> Estatisticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <NavLink to="/conta">
+        <MinhasFotos />
+        Minhas Fotos
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        Estatisticas
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        Adicionar foto
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair />
+        Sair
+      </button>
     </nav>
   );
 }
