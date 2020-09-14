d6df507637c182ec8a9b7c4b6794cbe57303c59b
Renan da silva santos <renan.serverti@gmail.com>
2020-09-14 19:32:24 -0300

    feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,7 +1,14 @@
 import React from "react";
+import FeedModal from "../../components/Feed/FeedModal";
+import FeedPhotos from "../../components/Feed/FeedPhotos";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhotos />
+    </div>
+  );
 }
 
 export default Feed;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Feed from "./Feed/Feed";
 
 function Home() {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <section className="container mainContainer">
+      <Feed />
+    </section>
   );
 }
 
