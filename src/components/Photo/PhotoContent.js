60cc5be1c462cb18bec17f4fdc09d4b29c6667bb
Renan da silva santos <renan.serverti@gmail.com>
2020-09-16 19:50:33 -0300

    modal terminado com funcao de fechar

--- src/components/Photo/PhotoContent.js
+++ src/components/Photo/PhotoContent.js
@@ -13,7 +13,7 @@ function PhotoContent({ data }) {
 
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -7,7 +7,9 @@ function Feed() {
 
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} />}
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
       <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
