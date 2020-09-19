8f6b887239081615d95feeee46de8bad09cdcaec
Renan da silva santos <renan.serverti@gmail.com>
2020-09-19 10:34:28 -0300

    api para comentario e estrutura do comentario

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { UserContext } from "../../context/UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
 
-function PhotoComments() {
-  return <div></div>;
+function PhotoComments({ id, comments }) {
+  const { login } = React.useContext(UserContext);
+  return <div>{login && <PhotoCommentsForm id={id} />}</div>;
 }
 
 export default PhotoComments;
---
+++ src/components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { COMMENT_POST } from "../../api";
+import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
+
+import useFetch from "../../hooks/useFetch";
+
+function PhotoCommentsForm({ id }) {
+  const [comment, setComment] = React.useState("");
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    await request(url, options);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+
+      <button>
+        <Enviar />
+      </button>
+    </form>
+  );
+}
+
+export default PhotoCommentsForm;
