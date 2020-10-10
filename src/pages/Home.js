24833be9e8276f439913ee1fbca6c1858ab9be05
Renan da silva santos <renan.serverti@gmail.com>
2020-10-10 19:32:52 -0300

    estrutura do loading iniciada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Feed from "./Feed/Feed";
 
+import Loading from "../components/Helper/Loading";
+
 function Home() {
   return (
     <section className="container mainContainer">
-      <Feed />
+      {/* <Feed /> */}
+
+      <Loading />
     </section>
   );
 }
